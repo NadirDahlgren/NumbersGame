5f3feb004de2f9e6e4b7d568dd548aedc36ce834
Nadir.Dahlgren@outlook.com <Nadir.Dahlgren@outlook.com>
2026-09-19 13:11:43 +0200

    Program is complete with comments and extra adjustments.

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -4,45 +4,106 @@
     {
         static void Main(string[] args)
         {
-            Random diceRoll = new Random(); // Declaring a new random variable for the correct numbre. 
-            int randomNumber = diceRoll.Next(1, 20); // Declaring the random number for the user. 
-            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
-            int guessNumber;
-            int guessCount = 0; // du kan ocmså skriva ihop dessa int variabel i en rad
-            
-            while (guessCount < ) // lägger en loop som håller koll på alla gissningar. 
+            //** INTRO **//
+
+            Random diceRoll = new Random();                                 // Declaring a new random variable for the correct number.
+            bool playAgain = true;                                          // Bool which decides if the game could repeat. 
+            while (playAgain)                                               // While-iteration for the game. 
             {
-                while (!int.TryParse(Console.ReadLine(), out guessNumber)) // loop för felinmatning
+                int randomAmount, randomNumber, userChoice, guessCount = 0; // Declaring different integer-variables for the program. 
+                bool hasWon = false;                                        // Bool for checking if the user has won. 
+                Console.WriteLine                                           // Game-menu
+                    ("Välkommen! Jag tänker på ett nummer mellan 1-20. Kan du gissa vilket? Du får fem försök.\n" +
+                    "\nOm du vill byta svårighetsgrad så kan du skriva antalet nummer som skall slumpas fram själv av datorn. " +
+                    "\n[1]. Gå vidare med samma svårighetsgrad " +
+                    "\n[2]. Välj en egen svårighetsgrad");
+                userChoice = GetValidNumber();                              // Checking input from the user. 
+
+            //** DIFFICULTY **//
+
+                if (userChoice == 2)
                 {
-                    Console.WriteLine("Du måste skriva ett heltal. Försök igen:");
+                    Console.WriteLine("Hur många nummer vill du att datorn ska slumpa mellan?");
+                    randomAmount = GetValidNumber();                         // Checking input from the user. 
+                    randomNumber = diceRoll.Next(1, randomAmount + 1);       // New variable with random-amount. 
+                    Console.Clear();
+                    Console.WriteLine($"Programmet slumpar ett tal mellan 1 och {randomAmount}.");
                 }
-                GuessCheck(guessNumber, randomNumber);
-                guessCount++;
+                else
+                {
+                    randomNumber = diceRoll.Next(1, 21);                    // Regular-value for the diceroll. 
+                }
 
+            //** GUESSING GAME **//
+
+                while (guessCount < 5)                                      // Iteration for the counting of the guesses. 
+                {
+                    Console.WriteLine("Ange ett tal för gissning:");
+                    hasWon = GuessCheck(randomNumber);                      // Method for checking the users guessing. 
+                    guessCount++;                                           // Add one to the guess count to keep track of the guesses. 
+
+                    if (hasWon)                                             // If guesscheck is true, break the iteration. 
+                    {
+                        break;                                              // Iteration is over.
+                    }
+                }
+                if (!hasWon)                                                // If user hasnt won in five rounds. 
+                {
+                    Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
+                }
+
+            //** PLAY AGAIN **//
+
+                Console.WriteLine("Vill du spela igen? Tryck 1 för Ja eller 2 för Nej."); 
+                int choice;                                                 // New int variable for the replay. 
+                choice = GetValidNumber();                                  // Checking input from the user. 
+                if (choice == 1)                                            // If the player chooses 1, the game loop starts again.
+                {
+                    playAgain = true;
+                    Console.Clear();
+                }
+                else                                                        // Or else, the game is over.
+                {
+                    playAgain = false;
+                }
             }
-            Console.WriteLine("goSpelet är slut. Du har gissat antal gissningar. ");
-
-
-
+            Console.WriteLine("Tack för att du spelade!");
         }
-
-        static void GuessCheck(int a, int b)
+        static bool GuessCheck(int randomNumber)                        // Bool-method with int as a parameter for checking the guesses. 
         {
-            if (a == b)
+            int userNumber;
+            userNumber = GetValidNumber();                                 // Checking input from the user.
+            if (randomNumber == userNumber)                                 // If the users input matches the random number, the user wins the game.
             {
-                Console.WriteLine("Grattis! Du har gissat rätt");
+                Console.WriteLine("Wohoo! Du klarade det!");
+                return true;                                                // Returns true from the method since the user has won the game.
             }
-            else if (a > b)
+            else if (userNumber < randomNumber && userNumber >= randomNumber - 2)   // Else-if statement for hinting the player. 
             {
-                Console.WriteLine("Försök igen! Talet är för stort.");
+                Console.WriteLine("Det bränns! Du gissade lite för lågt.");
             }
-            else
+            else if (userNumber > randomNumber && userNumber <= randomNumber + 2)  // Else-if statement for hinting the player. 
             {
-                Console.WriteLine("Försök igen! Talet är för litet");
+                Console.WriteLine("Det bränns! Du gissade lite för högt.");
             }
-
-
-
+            else if (userNumber < randomNumber)                             // Else-if statement for hinting the player. 
+            {
+                Console.WriteLine("Tyvärr, du gissade för lågt!");
+            }
+            else // Else statement for hinting the player. 
+            {
+                Console.WriteLine("Tyvärr, du gissade för högt!");
+            }
+            return false;                                                   // Bool method returns false if the player didnt get the correct answer.
+        }
+        static int GetValidNumber()                                     // Int-method to get the valid number from the user. 
+        {
+            int userNumber;
+            while (!int.TryParse(Console.ReadLine(), out userNumber))       // While-iteration for wrong input. 
+            {
+                Console.WriteLine("Du måste skriva ett heltal. Försök igen:");
+            }
+            return userNumber;                                              // Returns the integer. 
         }
     }
 }
